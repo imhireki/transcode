e316ea6aab98757380325b1a7195c3134fbe0c95
imHireki <imhireki@gmail.com>
2022-09-12 16:33:26 -0300

    [feat]: add rename_files script.

---
+++ rename_files.py
@@ -0,0 +1,26 @@
+#!/bin/python3.10
+
+import sys
+import os
+
+
+def get_file_extension(file_name: str):
+    splitted_file_name = file_name.split('.')
+    if len(splitted_file_name) > 1:
+        return '.' + splitted_file_name[-1]
+    return ''
+
+def rename_directory_files(directory: str, suffix: str = ''):
+    sorted_files = sorted(os.listdir(directory))
+
+    for file_id, file_name in enumerate(sorted_files, start=1):
+        absolute_file_name = f'{directory}/{file_name}'
+
+        file_extension = get_file_extension(file_name)
+        new_absolute_file_name = f'{directory}/{file_id}'\
+                                 f'{suffix}{file_extension}'
+
+        os.rename(absolute_file_name, new_absolute_file_name)
+
+if __name__ == '__main__':
+    rename_directory_files(sys.argv[1], sys.argv[2])
