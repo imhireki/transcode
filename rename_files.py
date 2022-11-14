49f18ce8f308f4de678e64bf6a97881a4a809e75
imHireki <imhireki@gmail.com>
2022-11-14 18:58:23 -0300

    update rename_files

--- rename_files.py
+++ rename_files.py
@@ -1,26 +1,27 @@
-#!/bin/python3.10
+#!/bin/python3
 
 import sys
 import os
 
 
-def get_file_extension(file_name: str):
+def get_file_extension(file_name: str) -> str:
     splitted_file_name = file_name.split('.')
     if len(splitted_file_name) > 1:
         return '.' + splitted_file_name[-1]
     return ''
 
-def rename_directory_files(directory: str, suffix: str = ''):
+def rename_directory_files(directory: str, prefix: str = '') -> None:
     sorted_files = sorted(os.listdir(directory))
 
     for file_id, file_name in enumerate(sorted_files, start=1):
         absolute_file_name = f'{directory}/{file_name}'
 
         file_extension = get_file_extension(file_name)
-        new_absolute_file_name = f'{directory}/{file_id}'\
-                                 f'{suffix}{file_extension}'
+        new_absolute_file_name = f'{directory}/{prefix}'\
+                                 f'{file_id}{file_extension}'
 
         os.rename(absolute_file_name, new_absolute_file_name)
 
 if __name__ == '__main__':
     rename_directory_files(sys.argv[1], sys.argv[2])
+
