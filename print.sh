75df34eb3421ac33f6598567eb8424ae35af01c5
imHireki <imhireki@gmail.com>
2023-02-14 17:01:59 -0300

    change the filename and keep the code dry

--- print.sh
+++ print.sh
@@ -1,21 +1,12 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-print_select() {
-    maim -s | \
-    tee ~/pictures/screenshots/select/$(date  +%Y_%m_%d-%H:%M:%S).png | \
-    xclip -selection clipboard -t image/png
-}
-
-print_screen() {
-    maim | \
-    tee ~/pictures/screenshots/$(date +%Y_%m_%d-%H:%M:%S).png | \
-    xclip -selection clipboard -t image/png
+function store_screenshot() {
+  tee ~/pictures/screenshots/$(date +%s).png | \
+  xclip -selection clipboard -t image/png
 }
 
 case $1 in
-    select)
-        print_select;; 
-    screen)
-        print_screen;;
+    select) maim -s -d 1 | store_screenshot;; 
+    screen) maim -d 1 | store_screenshot;;
 esac
 
