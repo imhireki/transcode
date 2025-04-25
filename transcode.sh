80f1db36bd8d4fc4b8f6418e245b9f03f1f84465
imhireki <hireki@proton.me>
2025-04-25 13:20:32 -0300

    [refactor]: replace deprecated arguments' module and functions with flags

--- transcode.sh
+++ transcode.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 source ./utils.sh
-source ./arguments.sh
+source ./flags.sh
 
 transcode() {
   media="$1"
@@ -9,39 +9,39 @@ transcode() {
 
   add_filename_to_json "$media"
 
-  subtitle_arguments=$(get_subtitle_arguments "$media")
-  video_arguments=$(get_video_arguments "$media")
-  audio_arguments=$(get_audio_arguments "$media")
+  video_flags=$(make_video_flags "$media")
+  audio_flags=$(make_audio_flags "$media")
+  subtitle_flags=$(make_subtitle_flags "$media")
 
   # There's no actions to be performed, return.
-  ! [[ "$video_arguments" =~ (h264) ]] \
-    && ! [[ "$audio_arguments" =~ (aac) ]] \
-    && ! [[ "$subtitle_arguments" =~ (filter_complex|disposition) ]] \
+  ! [[ "$video_flags" =~ (h264) ]] \
+    && ! [[ "$audio_flags" =~ (aac) ]] \
+    && ! [[ "$subtitle_flags" =~ (filter_complex|disposition) ]] \
     && return
 
   # BURNING SUBS
-  if [[ "$subtitle_arguments[@]" =~ (filter_complex) ]]; then
-    video_stream_index=$(echo "$video_arguments" | grep -Po "(?<=-map 0:)\d+")
+  if [[ "$subtitle_flags[@]" =~ (filter_complex) ]]; then
+    video_stream_index=$(echo "$video_flags" | grep -Po "(?<=-map 0:)\d+")
 
     # Remove mapping and set a codec with video options
-    video_arguments="-c:${video_stream_index} h264 -profile:v high " \
+    video_flags="-c:${video_stream_index} h264 -profile:v high " \
                     "-pix_fmt yuv420p -preset fast"
 
     # Replace [0:v:0] with the actual stream index [0:video_stream_index]
-    subtitle_arguments[1]=$(echo "${subtitle_arguments[1]}"\
+    subtitle_flags[1]=$(echo "${subtitle_flags[1]}"\
       | sed "s/\[0:v:0\]/\[0:${video_stream_index}\]/g")
 
-    # Argument order to burn sub
-    arguments=($video_arguments ${subtitle_arguments[@]} $audio_arguments)
+    # flag order to burn sub
+    flags=($video_flags ${subtitle_flags[@]} $audio_flags)
   else
-    # Argument order to copy sub
-    arguments=($video_arguments $audio_arguments ${subtitle_arguments[@]})
+    # flag order to copy sub
+    flags=($video_flags $audio_flags ${subtitle_flags[@]})
   fi
 
   output_filename=$(get_output_filename "$media" "$to_directory")
 
   ffmpeg -v quiet -stats -hide_banner -nostdin -i "$media" \
-    ${arguments[@]} "$output_filename" 2>> /tmp/transcode_stats
+    ${flags[@]} "$output_filename" 2>> /tmp/transcode_stats
 
   rm /tmp/transcode_stats
 }
