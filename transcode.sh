9d88512d74e510a72aa9c0b771e91e0dc0de0fd4
imhireki <hireki@proton.me>
2025-04-30 14:05:48 -0300

    [refactor]: read the flags as arrays; use has_pending_operations and make_burning_sub_flags

--- transcode.sh
+++ transcode.sh
@@ -11,39 +11,23 @@ transcode() {
   initialize_shared_counter
   initialize_state
 
-  video_flags=$(make_video_flags "$media")
-  audio_flags=$(make_audio_flags "$media")
-  subtitle_flags=$(make_subtitle_flags "$media")
+  read -ra video_flags < <(make_video_flags "$media")
+  read -ra audio_flags < <(make_audio_flags "$media")
+  read -ra subtitle_flags < <(make_subtitle_flags "$media")
 
-  # There's no actions to be performed, return.
-  ! [[ "$video_flags" =~ (h264) ]] \
-    && ! [[ "$audio_flags" =~ (aac) ]] \
-    && ! [[ "$subtitle_flags" =~ (filter_complex|disposition) ]] \
-    && return
+  has_pending_operations || return
 
-  # BURNING SUBS
-  if [[ "$subtitle_flags[@]" =~ (filter_complex) ]]; then
-    video_stream_index=$(echo "$video_flags" | grep -Po "(?<=-map 0:)\d+")
-
-    # Remove mapping and set a codec with video options
-    video_flags="-c:${video_stream_index} h264 -profile:v high " \
-                    "-pix_fmt yuv420p -preset fast"
-
-    # Replace [0:v:0] with the actual stream index [0:video_stream_index]
-    subtitle_flags[1]=$(echo "${subtitle_flags[1]}"\
-      | sed "s/\[0:v:0\]/\[0:${video_stream_index}\]/g")
-
-    # flag order to burn sub
-    flags=($video_flags ${subtitle_flags[@]} $audio_flags)
+  if is_burning_sub; then
+    read -ra video_flags < <(make_burning_sub_video_flags)
+    flags=( "${subtitle_flags[@]}" "${video_flags[@]}" "${audio_flags[@]}" )
   else
-    # flag order to copy sub
-    flags=($video_flags $audio_flags ${subtitle_flags[@]})
+    flags=( "${video_flags[@]}" "${audio_flags[@]}" "${subtitle_flags[@]}" )
   fi
 
   output_filename=$(get_output_filename "$media" "$to_directory")
 
-  ffmpeg -v quiet -stats -hide_banner -nostdin -i "$media" \
-    ${flags[@]} "$output_filename" 2>> /tmp/transcode_stats
+  ffmpeg -v quiet -stats -hide_banner -nostin \
+    -i "$media" "${flags[@]}" "$output_filename"
 
   cleanup
 }
