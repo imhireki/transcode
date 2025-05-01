4221ffe663cf3dfddd4079ea9daecd76d3f8966a
imhireki <hireki@proton.me>
2025-05-01 17:35:14 -0300

    [fix]: make sure get_output_filename can handle all types of paths and can dynamically set formats

--- utils.sh
+++ utils.sh
@@ -83,47 +83,24 @@ get_output_filename() {
   media="$1"
   to_directory="$2"
 
-  # Make the input directory in the output directory
-  media_base_dir=$(awk -F "/" '{print $(NF-1)}' <<< "$media")
-  mkdir -p "${to_directory}${media_base_dir}/"
+  filename=$(basename "$media")  # a/b/c.mkv -> c.mkv
+  title="${filename%.*}"
+  extension="${filename##*.}"
 
-  filename=$(basename "$media")
-  output="${to_directory}${media_base_dir}/${filename}"
-
-  format_name=$(ffprobe -v quiet -show_entries format=format_name \
-     -print_format json "$media"| jq -r ".format.format_name")
-
-  # Echo output non-mkv/mp4 as mkv
-  [[ "$format_name" =~ ^(matroska,webm|mov,mp4,m4a,3gp,3g2,mj2)$ ]] \
-    && echo "$output" || sed "s/\.\w\+$/.mkv/" <<< "$output"
-}
-
-save_working_dirs_to_json() {
-  from_dir="$1"
-  to_dir="$2"
-
-  from_dir_basename=$(basename "$from_dir")
-
-  input_dir="$from_dir"
-  output_dir="${to_dir}${from_dir_basename}/"
-
-  object=$(jq -n \
-    --arg input_dir "$input_dir" \
-    --arg output_dir "$output_dir" \
-    '{ "INPUT_DIR": $input_dir, "OUTPUT_DIR": $output_dir }'
+  format_name=$(
+    ffprobe -v quiet -show_entries format=format_name \
+    -print_format json  "$media" | jq -r ".format.format_name"
   )
-  echo "$object" > /tmp/transcode_data.json
-}
-
-add_filename_to_json() {
-  filename="$1"
-
-  if [ -f /tmp/transcode_data.json ]; then
-    object=$(/bin/cat /tmp/transcode_data.json \
-      | jq --arg filename "$filename" '.FILENAME= $filename')
-  else
-    object=$(jq -n --arg filename "$filename" '{"FILENAME":$filename}')
+  if ! match_attribute "$format_name" "$SUPPORTED_FORMATS"; then
+    extension="$PREFERRED_EXTENSION"
   fi
 
-  echo "$object" > "/tmp/transcode_data.json"
+  # Add .transcode. if the output is going to where the input is
+  media_dir=$(dirname "$media")
+  if [[ "$to_directory" == "$media_dir" ]]; then
+    echo "${title}.transcoded.${extension}"
+  else
+    mkdir -p "$to_directory"
+    echo "${to_directory}/${title}.${extension}"
+  fi
 }
