eeea095f745e4709883fb679ea161502763089da
imhireki <hireki@proton.me>
2025-05-07 13:00:53 -0300

    [refactor]: split the transcode file into build_ordered_flags and transcode functions

--- transcode.sh
+++ transcode.sh
@@ -3,15 +3,16 @@
 source ./utils.sh
 source ./flags.sh
 
-transcode() {
+build_ordered_flags() {
   media="$1"
-  output="$2"
+
+  initialize_state
 
   read -ra video_flags < <(make_video_flags "$media")
   read -ra audio_flags < <(make_audio_flags "$media")
   read -ra subtitle_flags < <(make_subtitle_flags "$media")
 
-  has_pending_operations || return
+  has_pending_operations || { cleanup_state; return; }
 
   if is_burning_sub; then
     read -ra video_flags < <(make_burning_sub_video_flags)
@@ -20,29 +21,42 @@ transcode() {
     flags=( "${video_flags[@]}" "${audio_flags[@]}" "${subtitle_flags[@]}" )
   fi
 
-  ffmpeg -v quiet -hide_banner -nostdin -progress pipe:1 \
-    -i "$media" "${flags[@]}" "$output" | parse_progress
+  cleanup_state
+
+  echo "${flags[@]}"
 }
+
+transcode() {
+  media="$1"
+  output="$2"
+
+  update_json ".duration" "$(get_duration "$media" )" "$METADATA"
+
+  # Get ordered flags for ffmpeg
+  read -ra ordered_flags < <(build_ordered_flags "$media")
+  [[ "${#ordered_flags}" -eq 0 ]] && return 1
+
+  ffmpeg -v quiet -hide_banner -nostdin -progress pipe:1 \
+    -i "$media" "${ordered_flags[@]}" "$output" | parse_progress
+
+  # The file's been processed. Increase counter.
+  num_output_files=$(jq -r ".num_output_files" "$METADATA")
+  ((num_output_files++))
+  update_json ".num_output_files" "$num_output_files" "$METADATA"
+}
+
 
 media_path=$(realpath "$1" 2>/dev/null || echo "")
 to_directory=$(realpath -m "$2" 2>/dev/null || echo "$PWD")
 
-files=$(find "$media_path" -maxdepth 1 -type f 2>/dev/null | sort)
-readarray -t files < <(find "$media_path" -maxdepth 1 -type f 2>/dev/null | sort)
-
 initialize_metadata
+
+readarray -t files < <(find "$media_path" -maxdepth 1 -type f 2>/dev/null | sort)
 update_json ".num_input_files" "${#files[@]}" "$METADATA"
 
 for media in "${files[@]}"; do
-  update_json ".duration" "$(get_duration "$media" )" "$METADATA"
-
-  initialize_state
   output=$(get_output_filename "$media" "$to_directory")
   transcode "$media" "$output"
-  cleanup_state
-
-  num_output_files=$(jq -r ".num_output_files" "$METADATA")
-  ((num_output_files++))
-  update_json ".num_output_files" "$num_output_files" "$METADATA"
 done
+
 cleanup_metadata
