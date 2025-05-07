90e9b8b42fe737abb20e9f81008a17d2f77e355d
imhireki <hireki@proton.me>
2025-05-07 15:14:39 -0300

    [refactor]: limit the variable's scope at flags.sh

--- flags.sh
+++ flags.sh
@@ -4,13 +4,15 @@ source ./config.sh
 source ./utils.sh
 
 make_audio_flags() {
-  media="$1"
-  flags=()
+  local media="$1"
+  local flags=()
+  local stream
 
   while IFS= read -r stream; do
+
+    local index codec shared_counter
     index=$(jq -r ".index" <<< "$stream")
     codec=$(jq -r ".codec_name" <<< "$stream")
-
     shared_counter=$(next_from_shared_counter)
 
     if match_attribute "$codec" "$SUPPORTED_AUDIO_CODECS"; then
@@ -25,20 +27,25 @@ make_audio_flags() {
 }
 
 make_burning_sub_video_flags() {
+  local output_index flags
   output_index=$(jq ".video.output_index" "$STATE")
   flags=( "-c:${output_index}" "$VIDEO_ENCODING_FLAGS" )
+
   echo "${flags[*]}"
 }
 
 make_video_flags() {
-  media="$1"
+  local media="$1"
+  local stream
 
   while IFS= read -r stream; do
+    local codec
     codec=$(jq -r ".codec_name" <<< "$stream")
 
     # Skip covers
     match_attribute "$codec" "$UNSUPPORTED_COVERS" && continue
 
+    local index profile shared_counter
     index=$(jq -r ".index" <<< "$stream")
     profile=$(jq -r ".profile" <<< "$stream")
     shared_counter=$(next_from_shared_counter)
@@ -59,10 +66,13 @@ make_video_flags() {
 }
 
 group_subs_by_format() {
-  media="$1"
-  groups='{"text": [], "image": []}'
+  local media="$1"
+  local groups='{"text": [], "image": []}'
+  local stream
 
   while IFS= read -r stream; do
+
+    local codec index
     codec=$(jq -r ".codec_name" <<< "$stream")
     index=$(jq -r ".index" <<< "$stream")
 
@@ -77,20 +87,25 @@ group_subs_by_format() {
 }
 
 make_text_sub_flags() {
-  media="$1"
-  indexes="$2"
-  flags=()
+  local media="$1"
+  local indexes="$2"
+  local flags=()
 
   # The loop would run once, even if the indexes is empty
   [[ -z "$indexes" ]] && return
 
+  local index
+
   while IFS= read -r index; do
+
+    local stream shared_counter
     stream=$(select_stream_by_index "$media" "$index")
     shared_counter=$(next_from_shared_counter)
 
     flags+=("-map 0:${index} -c:${shared_counter} copy")
 
     # Remove forced disposition from the sub
+    local forced
     forced=$(jq -r ".disposition.forced" <<< "$stream")
     if [[ "$forced" == "1" ]]; then
       flags+=("-disposition:${shared_counter} -forced")
@@ -102,19 +117,20 @@ make_text_sub_flags() {
 }
 
 make_image_sub_flags() {
-  media="$1"
-  indexes="$2"
-
-  subs_with_size=()
+  local media="$1"
+  local indexes="$2"
+  local subs_with_size=()
 
   # The loop would run once, even if the indexes is empty
   [[ -z "$indexes" ]] && return
 
   update_json ".transcoding.subtitle" true "$STATE"
+  local index
 
   while IFS= read -r index; do
-    stream=$(select_stream_by_index "$media" "$index")
 
+    local stream size
+    stream=$(select_stream_by_index "$media" "$index")
     size=$(
       echo "$stream" | jq -r ".tags" |
         grep -i -Po '".*byte.*": "\d+"' |
@@ -134,16 +150,18 @@ make_image_sub_flags() {
   done <<< "$indexes"
 
   # Sort by biggest size
+  local sorted_subs_with_size
   readarray -t sorted_subs_with_size < <(
     printf "%s\n" "${subs_with_size[@]}" | sort -k2 -rn
   )
 
+  local max_size_sub_index input_video_index
   max_size_sub_index=$(
     echo "${sorted_subs_with_size[*]}" | head -n 1 | cut -d" " -f 1
   )
-
   input_video_index=$(jq ".video.input_index" "$STATE")
 
+  local overlay_filter_flags
   overlay_filter_flags=(
     -filter_complex
     "[0:${input_video_index}][0:${max_size_sub_index}]overlay[v]"
@@ -154,10 +172,12 @@ make_image_sub_flags() {
 }
 
 make_subtitle_flags() {
-  media="$1"
+  local media="$1"
 
+  local sub_groups
   sub_groups=$(group_subs_by_format "$media")
 
+  local text_sub_indexes text_sub_flags
   text_sub_indexes=$(jq -r ".text[]" <<< "$sub_groups")
   text_sub_flags=$(make_text_sub_flags "$media" "$text_sub_indexes")
 
@@ -167,6 +187,7 @@ make_subtitle_flags() {
   # Return if it's not supposed to burn any image-based sub
   [[ $BURN_IMAGE_SUBTITLE == true ]] || return
 
+  local image_sub_indexes image_sub_flags
   image_sub_indexes=$(jq -r ".image[]" <<< "$sub_groups")
   image_sub_flags=$(make_image_sub_flags "$media" "$image_sub_indexes")
 
